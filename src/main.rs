ace4a994ba7f5b856e462389623147bba5526ded
djnovin <hello@novinnoori.com>
2024-10-09 12:37:32 +1100

    Initialise Actix Web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+    })
+    .bind(("127.0.0.1", 3000))?
+    .run()
+    .await
 }
