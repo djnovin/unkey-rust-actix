dfea0cad90d074b4ff961b495a60300d8bd6ef60
djnovin <hello@novinnoori.com>
2024-10-09 12:43:11 +1100

    Add Port

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use actix_web::{App, HttpServer};
+use std::env;
+use dotenv::dotenv;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
     HttpServer::new(|| {
         App::new()
     })
-    .bind(("127.0.0.1", 3000))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
